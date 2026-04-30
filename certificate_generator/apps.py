2de2fb530c2ba2f84170067752d9c0f3077dbacf
Andrea K. <andrea.kyurchiev@flaxandteal.co.uk>
2026-04-30 16:00:07 +0300

    chore: add is_arches_app flag

--- certificate_generator/apps.py
+++ certificate_generator/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class CertificateGeneratorConfig(AppConfig):
     name = "certificate_generator"
     verbose_name = "Certificate Generator"
-
+    is_arches_application = True
+    
     def ready(self):
         pass
